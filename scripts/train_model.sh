eae59e29fd0bb5ac9711095e1b2925b997c01307
VillaHsu <32631423+VillaHsu@users.noreply.github.com>
2021-09-16 16:02:12 +0800

    Update train_model.sh

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -x
 
 DATASET=${1:-dna}
-N_ITER=1000
+N_ITER=10000
 BUFFER_SIZE=30
-BATCH_SIZE=1
+BATCH_SIZE=12
 RUN_DIR="saved_models/${DATASET}"
 PATH_DATASET_ALL_CSV="data/csvs/${DATASET}.csv"
 PATH_DATASET_TRAIN_CSV="data/csvs/${DATASET}/train.csv"
-PATH_DATASET_VAL_CSV="data/csvs/${DATASET}/val.csv"
+#PATH_DATASET_VAL_CSV="data/csvs/${DATASET}/val.csv"
 GPU_IDS=${2:-0}
 CLASS=CziDataset #CziDataset
-MODEL=KiUnet #fnet_nn_3d or KiUnet
+MODEL=fnet_nn_3d #fnet_nn_3d or KiUnet
 
 python scripts/python/split_dataset.py ${PATH_DATASET_ALL_CSV} "data/csvs" --train_size 0.75 -v
 python train_model.py \
        --n_iter ${N_ITER} \
        --path_dataset_csv ${PATH_DATASET_TRAIN_CSV} \
-       --path_dataset_val_csv ${PATH_DATASET_VAL_CSV} \
+       #--path_dataset_val_csv ${PATH_DATASET_VAL_CSV} \
        --buffer_size ${BUFFER_SIZE} \
        --buffer_switch_frequency 2000000 \
        --batch_size ${BATCH_SIZE} \
